641991d790fae9cbca0b16ff703b3405ab46e00e
yangxia605 <yangxia605@gmail.com>
2020-06-22 16:38:51 +0800

    搭建项目，可以登录

---
+++ web/src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/Login'
+
+Vue.use(Router)
+
+import Layout from '../layout'
+
+export const constantRoutes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/dashboard',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('../components/dashboard/index'),
+      meta: { title: '首页', icon: 'dashboard' }
+    }]
+  }
+]
+
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
